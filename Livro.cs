c0db63783bc2748086b2869efe4ce26fea5a73a8
Daniel Sousa <FH0102@t-systems.com.br>
2021-07-28 22:55:14 -0300

    [Feat] Adicionando documentos atraves de objetos

--- Livro.cs
+++ Livro.cs
@@ -12,12 +12,11 @@ namespace ExemplosMongoDB
     {
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-
-        public string Título { get; set; }
+        public string Titulo { get; set; }
         public string Autor { get; set; }
         public int Ano { get; set; }
-        public int Páginas { get; set; }
-        public List<string> Assunto { get; set; }
+        public int Paginas { get; set; }
+        public List<string> Assuntos { get; set; }
         
     }
 }
